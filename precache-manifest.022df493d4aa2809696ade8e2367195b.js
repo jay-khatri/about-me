b5e36686453f1b474ff4a1330ca1b0303ad18ed9
Jay Khatri <khatri.florida@gmail.com>
2018-10-25 17:00:22 -0500

    Updates

---
+++ precache-manifest.022df493d4aa2809696ade8e2367195b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/about-me/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "cb9bbd4c194e1c136ea7",
+    "url": "/about-me/static/js/runtime~main.cb9bbd4c.js"
+  },
+  {
+    "revision": "71fe016c79e1d2ae7f53",
+    "url": "/about-me/static/js/main.71fe016c.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/about-me/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "71fe016c79e1d2ae7f53",
+    "url": "/about-me/static/css/main.0b992445.chunk.css"
+  },
+  {
+    "revision": "bb0fb988240d2cac4749abad5a63922a",
+    "url": "/about-me/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a417e0a6a9202d358090b482f786f08a.js"
+  "/about-me/precache-manifest.022df493d4aa2809696ade8e2367195b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/about-me/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
